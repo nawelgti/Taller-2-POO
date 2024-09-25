17326154f4818760dc5dfb59564363e251cb89d8
nawelgti <n.gutierrez08@ufromail.cl>
2024-09-25 10:46:07 -0300

    tercer commit

---
+++ src/test/java/org/example/RegistroTest.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+public class RegistroTest {
+    @Test
+    void testHayCupo() {
+
+    }
+
+    @Test
+    void testObtenerUltimoEspacio() {
+
+    }
+}
